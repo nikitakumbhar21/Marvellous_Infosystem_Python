a0a94ecb33c1c54dc9017c86821b52efbe607fd9
Nikita Kumbhar <kumbharniki21.nk@gmail.com>
2026-06-23 00:54:15 +0530

    Added two programs from assigment 1

---
+++ Assig1Prog1.py
@@ -0,0 +1,23 @@
+# Write a program that accepts two numbers from user and print their: 
+# Addition
+# Substraction
+# Multiplication
+# Division
+# -------------------------------------------------------------------------------
+
+print("Enter First Number: ")
+No1 = int(input())
+print("Enter Second Number: ")
+No2 = int(input())
+
+Add = No1 + No2
+print("Addition of Two Number is: ", Add)
+
+Sub = No1 - No2
+print("Substraction of Two Number is: ", Sub)
+
+Mult = No1 * No2
+print("Multiplication of Two Number is: ", Mult)
+
+Div = No1 / No2
+print("Division of Two Number is: ", Div)
---
+++ Assig1Prog2.py
@@ -0,0 +1,11 @@
+# Write a program to take user's name, age  & display that.
+# ---------------------------------------------------------------
+
+print("Enter Name: ")
+name = input()
+
+print("Enter Age: ")
+Age = int(input())
+
+# Display the message with the calculated for next year
+print("Hello",name ,"you will turn",Age + 1, "next year")
